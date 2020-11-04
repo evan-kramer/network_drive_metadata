af348e78968c6365c8168cfbfba0eb9bf6977ed5
Evan Kramer <kramer.evan@gmail.com>
2020-11-04 18:52:16 -0500

    Added details to README about problem and solution, and added output and email notification features

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Network Drive Metadata
+
+
+## Problem
+- We're running out of space on the network drive.
+- It costs money to provision/maintain these servers.
+- We're not sure what is taking up much of that 
+
+## Potential Solutions
+- We can remove old files.
+- We can zip together old versions.
+- We can migrate files to other (lower-cost) locations (e.g., Teams SharePoint, etc.).
+
+## Hangups
+- We need to know what is taking up space, what is being used/accessed, and what we can safely remove.
--- network_drive_metadata.R
+++ network_drive_metadata.R
@@ -1,16 +1,53 @@
 # Network Drive Metadata
 # Evan Kramer
 
-library(tidyverse); library(lubridate); library(readxl)
+# Set up
+library(tidyverse); library(lubridate); library(readxl); library(curl)
+setwd("X:/Analysis Team/Development/X Drive Crawl/X-Files/temp")
+
+# Get list of all files with timestamp
 t1 = now()
 file_list = list.files("X:/ACGR", recursive = T)
-d = now() - t1
-print(d)
+d1 = str_remove(difftime(now(), t1, units = "mins"), "Time") %>% 
+  as.numeric() %>% 
+  round(2)
+
+# Get metadata for all files with timestamp
+t2 = now()
 data = map(
-  .x = str_c("X:/ACGR/", file_list)[1:100],
-  .f = ~file.info(str_c("X:/ACGR/"), .x)
+  .x = str_c("X:/ACGR/", file_list)[10],
+  .f = ~tibble(file = .x, size = file.size(.x), isdir = file.info(.x)$isdir, 
+               ctime = file.info(.x)$ctime, mtime = file.mtime(.x), 
+               timestamp = now())
 ) 
+file_info = tibble()
+for(i in 1:length(data)) {
+  file_info = bind_rows(file_info, data[[i]])
+}
+d2 = str_remove(difftime(now(), t2, units = "mins"), "Time") %>% 
+  as.numeric() %>% 
+  round(2)
 
-  
+# Output file
+write_csv("network_drive_metadata.csv", na = "")
 
+# Email confirmation
+m = 'From: evan.kramer@dc.gov
+To: evan.kramer@dc.gov
+Subject: SUCCESS: Network Drive Metadata
 
+The network_drive_metadata.R script ran successfully.
+It took d1 minutes to inventory all files.
+It took d2 minutes to get metadata for all files.'
+
+# Send email with error or run script
+send_mail(
+  mail_from = readRegistry("Environment", hive = "HCU")$api_uid,
+  mail_rcpt = 'anthonys.graham@dc.gov',
+  message = str_replace(m, "d1", as.character(d1)) %>% 
+    str_replace("d2", as.character(d2)),
+  smtp_server = "smtp://smtp.office365.com:587",
+  username = readRegistry("Environment", hive = "HCU")$api_uid,
+  password = readRegistry("Environment", hive = "HCU")$qb_api_pwd,
+  use_ssl = "force"
+)
